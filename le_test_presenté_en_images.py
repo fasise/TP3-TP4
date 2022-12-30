7a10dab0dca53a8ecba5ade76f7a55c12b74d1f8
fasise <compaorefasise@gmail.com>
2022-12-30 22:47:38 +0100

    Les tests que j'ai fait

---
+++ le_test_presenté_en_images.py
@@ -0,0 +1,12 @@
+ 
+from PIL import Image
+  
+
+im = Image.open(r"images de test\test home.png") 
+im1 = Image.open(r"images de test\test game controller.png") 
+im2 = Image.open(r"images de test\test game controller sur locallhost.png") 
+  
+
+im.show()
+im1.show()
+im2.show()
